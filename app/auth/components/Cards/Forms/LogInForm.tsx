2673124ab8f5ad5b78cf1196865c99b1574e4ea5
Jose Lozano <josecamilo.lozanocet@ucalgary.ca>
2023-11-22 13:22:53 -0700

    Revert "Auth Works with email"
    This reverts commit 3f6478f080464055be47263eb4cc5fb1d98c6be2.

--- app/auth/components/Cards/Forms/LogInForm.tsx
+++ app/auth/components/Cards/Forms/LogInForm.tsx
@@ -1,53 +1,27 @@
 "use client";
 
-import { loginWithEmailAndPassword } from '@/app/auth/actions';
 import { containsSQLInjection } from '@/lib/utils';
-import { redirect } from 'next/navigation';
 import toast from 'react-hot-toast';
 
 export default function LogInForm() {
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
 
         const entries = Object.fromEntries(formData.entries());
         const email = String(entries.email);
-        const password = String(entries.password);
+        const password1 = String(entries.password);
 
-        if (password.length < 6) {
+        if (password1.length < 6) {
             toast.error('Password must be at least 6 characters');
             return;
         }
 
-        if (containsSQLInjection(password)) {
+        if (containsSQLInjection(password1)) {
             toast.error('Invalid symbol in password');
             return;
         }
-
-        const data = {
-            email: String(entries.email),
-            password: String(entries.password),
-        };
-
-
-        const result = await loginWithEmailAndPassword(data);
-
-        console.log(result);
-
-        const { error } = JSON.parse(result);
-
-        if (error) {
-            toast.error(error.message);
-            return;
-        }
-
-        else {
-            toast.success('Logged In successfully');
-            redirect('/home');
-            return;
-        }
-
     }
 
     return (
--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import AuthForm from "./components/AuthForm";
 import { readUserSession } from "@/lib/actions";
 import { redirect } from "next/navigation";
-
 import SignInPage from "./components/SignInPage";
 
 export default async function page() {
 	const { data: userSession } = await readUserSession();
 
 	if (userSession.session) {
-		return redirect("/");
+		return redirect("/dashboard");
 	}
 	return (
 		<div className="flex flex-col items-center m-0 min-h-[100vh] justify-center">
