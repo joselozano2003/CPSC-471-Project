27328751a1faf98dff30477f045bbdcc420600c7
jaie24 <jiayiii_jaie@hotmail.com>
2023-12-09 03:23:27 -0700

    welcome page is looking VERY cute

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,34 @@ import { SignInButton, SignOutButton } from '@/components/buttons'
 import Image from 'next/image'
 import Link from 'next/link'
 import './pages.css'
+import homepageBackground from "public/welcome-page.jpg"
+
 
 export default function Home() {
+
+	const bgStyling = {
+        backgroundImage: `url(${homepageBackground.src})`,
+        backgroundSize : "100%",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+    
+    }
     return (
+		
+
 		<div>
-			<h1 className="welcome"> Welcome to the Hospital Database </h1>
-			<SignInButton />
+			<div className="background-wrapper" style={bgStyling}>
+      <div className="homepage-container">
+      <h1 className="welcome"> Welcome to the Hospital Database </h1>
+			<div className="button-container">
+      <SignInButton />
 			<SignOutButton />
+      </div>
+      </div>
+      </div>
+      
+			
+      
 
 		
 		</div>
